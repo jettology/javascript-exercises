ce850ebea8958e11a2c083a68b1dc5504762012e
Jettology <bsi.s.park@gmail.com>
2023-01-05 01:16:44 -0600

    Finish Temperature Conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+// C = F * 9/5 + 32
+// F = (C - 32) * 5/9
+
+const convertToCelsius = function(f_temp) {
+  return Math.round( ((f_temp - 32) * 5 / 9 ) * 10 ) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c_temp) {
+  return Math.round( (c_temp * 9 / 5 + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
