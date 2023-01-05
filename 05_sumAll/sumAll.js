8334536230604a2998a274548410cfbf5c9f683f
Jettology <bsi.s.park@gmail.com>
2023-01-05 00:32:42 -0600

    Finish Sum All exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    // my original solution for the check (still works)
+    if (start < 0 || end < 0 || typeof start != "number" || typeof end != "number") {
+        return "ERROR";
+    }
+
+    // another way for typeof check
+    /*
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    */
+
+    if (start > end) {
+        const temp = end
+        end = start;
+        start = temp;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
