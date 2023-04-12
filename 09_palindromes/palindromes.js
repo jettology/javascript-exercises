9b6066dba4e76501133738de14181e2c7acf6152
Jettology <bsi.s.park@gmail.com>
2023-04-12 15:52:29 +1000

    Finish Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    const regex = /[^A-Za-z0-9]/g;
 
+    // set lowercase
+    myString = myString.toLowerCase();
+    // remove any character that is not alphanumeric
+    myString = myString.replace(regex, '');
+    
+    let revString = myString.split("").reverse().join("");
+
+    return myString === revString;
 };
 
 // Do not edit below this line
