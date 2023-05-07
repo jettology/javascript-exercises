6f23f084c55038b31b07bf43fc625c8c86e8e758
Jettology <bsi.s.park@gmail.com>
2023-05-06 23:45:49 -0500

    Finish GetTheTitles and FindTheOldest exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    // Works but not ideal:
+
+    // const titles = [];
+    // for (let i=0; i<books.length; i++) {
+    //     titles[i] = books[i].title;
+    // }
+
+    // Better way:
+
+    const titles = books.map(book => book.title);
+
+    return titles;
 };
 
 // Do not edit below this line
